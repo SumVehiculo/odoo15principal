679757e58bf5b040a4968c60b7d13ee3c8ccc364
SumVehiculo <144273047+SumVehiculo@users.noreply.github.com>
2023-10-20 16:21:57 -0500

    Add files via upload

---
+++ report_sale_it/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "REPORTE VENTAS",
+    'summary': """
+        REPORTE VENTAS
+    """,
+    'description': """
+       REPORTE VENTAS
+    """,
+    'author': "ITGROUP",
+    'website': "",
+    'category': 'sale',
+    'version': '15.0.0.1',
+    'depends': [
+        'sale',
+    ],
+    'data': [
+        'views/sale_report_templates.xml'
+    ],
+    'application': True,
+    'license': 'LGPL-3',
+}
