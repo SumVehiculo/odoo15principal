72cef470d4e6b27a4c2cd121820c6e806a829ee9
Bertha Maribel Chávez Salazar <73680458+Bertha-cs@users.noreply.github.com>
2023-10-20 13:30:59 -0500

    Add files via upload

---
+++ purchase_report/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- encoding: utf-8 -*-
+{
+	'name': 'Reporte de compras',
+	'category': 'purchase',
+	'author': 'ITGRUPO,berth',
+	'depends': ['purchase'],
+	'version': '1.0',
+	
+	'auto_install': False,
+	'demo': [],
+	'data':	[
+        'reports/report_purchase_order.xml'
+        ],
+	'installable': True,
+	'license': 'LGPL-3'
+}
