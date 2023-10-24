d860b24cc00950638681f1c2ed74f92e545aa836
Bertha Maribel Chávez Salazar <73680458+Bertha-cs@users.noreply.github.com>
2023-10-24 15:17:49 -0500

    Add files via upload

---
+++ groups_varios/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- encoding: utf-8 -*-
+{
+	'name': 'Grupos varios',
+	'author': 'ITGRUPO,berth',
+	'depends': ['account','purchase','purchase_stock'],
+	'version': '1.0',
+	'auto_install': False,
+	'demo': [],
+	'data':	[
+		'security/security.xml',
+		'views/grupo.xml'
+	],
+	'installable': True,
+	'license': 'LGPL-3'
+}
