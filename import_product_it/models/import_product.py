2c97dfb05183ca7ecef8a784e470cb8e31cfaff4
Glenda Julia <glenda.merma@tecsup.edu.pe>
2023-11-01 01:20:01 -0500

    a

--- import_product_it/models/import_product.py
+++ import_product_it/models/import_product.py
@@ -2,7 +2,7 @@ from odoo import models, fields, exceptions, api, _
 import tempfile
 import binascii
 import xlrd
-from odoo.exceptions import Warning, UserError
+from odoo.exceptions import UserError
 import base64
 
 
@@ -23,7 +23,7 @@ class ImportProductIt(models.TransientModel):
 			result = []
 			workbook = xlrd.open_workbook(fp.name)
 		except Exception:
-				raise UserError("Sube un archivo .xlsx!") 
+				raise UserError("Sube un archivo .xlsx!")
 		sheet = workbook.sheet_by_index(0)
 		for row_no in range(sheet.nrows):
 			val = {}
@@ -97,7 +97,7 @@ class ImportProductIt(models.TransientModel):
 			res = {}
 			workbook = xlrd.open_workbook(fp.name)
 		except Exception:
-				raise UserError("Sube un archivo .xlsx!") 
+				raise UserError("Sube un archivo .xlsx!")
 		sheet = workbook.sheet_by_index(0)
 		for row_no in range(sheet.nrows):
 			val = {}
@@ -125,14 +125,12 @@ class ImportProductIt(models.TransientModel):
 								})
 					res = self.product_create(values)
 				else:
-					product_categ_obj = self.env['product.category'].search([])
+					product_categ_obj = self.env['product.category']
 					product_uom_obj = self.env['uom.uom']
 					if line[2]=='':
 						raise UserError('Campo CATEGORIA no puede estar vacío')
 					else:
-						for elem in product_categ_obj:
-							if elem.name_get()[0][1] == line[2]:
-								categ_id = elem
+						categ_id = product_categ_obj.search([('name','=',line[2])])
 					if line[3] == 'Consumible':
 						type ='consu'
 					elif line[3] == 'Servicio':
@@ -199,7 +197,7 @@ class ImportProductIt(models.TransientModel):
 												'property_account_income_id':income_account_id,
 												'property_account_expense_id': expense_account_id})
 						else:
-							raise UserError(('Producto "%s" no encontrado.') % line[1]) 
+							raise UserError('Producto "%s" no encontrado.'% line[1])
 					else:
 						product_ids = self.env['product.template'].search([('name','=', line[0])])
 						if product_ids:
@@ -219,7 +217,7 @@ class ImportProductIt(models.TransientModel):
 												'property_account_income_id':income_account_id,
 												'property_account_expense_id': expense_account_id})
 						else:
-							raise UserError(('Producto %s no encontrado.') % line[0])  
+							raise UserError('Producto %s no encontrado.'% line[0])  
 	
 					
 		return self.env['popup.it'].get_message('SE IMPORTARON LOS PRODUCTOS DE MANERA CORRECTA.')
@@ -232,16 +230,13 @@ class ImportProductIt(models.TransientModel):
 		if values.get('categ_id')=='':
 			raise UserError('Campo CATEGORIA no puede estar vacío.')
 		else:
-			categ_id = False
-			xproduct_categ_obj = self.env['product.category'].search([])
-			if values.get('categ_id')=='':
-				raise UserError('Campo CATEGORIA no puede estar vacío')
-			else:
-				for elem in product_categ_obj:
-					if elem.name_get()[0][1] == line[2]:
-						categ_id = elem
-			if categ_id == False:
-				raise UserError('No existe la categoria: ' + values.get('categ_id'))
+			categ_id = product_categ_obj.search([('complete_name','=',values.get('categ_id'))])
+			if categ_id :
+				categ_id = categ_id
+				
+			else :
+				raise UserError('No existe la Categoria %s.'% values.get('categ_id'))  
+		
 		if values.get('type') == 'Consumible':
 			type ='consu'
 		elif values.get('type') == 'Servicio':
@@ -249,7 +244,7 @@ class ImportProductIt(models.TransientModel):
 		elif values.get('type') == 'Almacenable':
 			type ='product'
 		else:
-			raise UserError(('%s no es un Tipo de Producto.') % values.get('type'))
+			raise UserError('%s no es un Tipo de Producto.' % values.get('type'))
 		
 		if values.get('uom_id')=='':
 			uom_id = 1
@@ -318,7 +313,7 @@ class ImportProductIt(models.TransientModel):
 		if account_search:
 			return account_search.id
 		else:
-			raise UserError(('No existe una Cuenta con el Codigo "%s" en esta Compañia') % code)
+			raise UserError('No existe una Cuenta con el Codigo "%s" en esta Compañia' % code)
 
 	def download_template(self):
 		return {
