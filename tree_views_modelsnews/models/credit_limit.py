c8298ba26124856b886d947d5d1cfbf7480dd760
SumVehiculo <144273047+SumVehiculo@users.noreply.github.com>
2023-11-07 14:15:24 -0500

    Add files via upload

--- tree_views_modelsnews/models/credit_limit.py
+++ tree_views_modelsnews/models/credit_limit.py
@@ -14,10 +14,10 @@ class PurchaseReport(models.Model):
 
 #    account_analytic_id = fields.Many2one("account.analytic.account",string="Cuenta Analítica")
     analytic_tag_ids = fields.Many2many("account.analytic.tag",string="Etiquetas Analíticas")
-
+    analytic_tag_ids_name = fields.Char(string="Etiquetas Analíticas")
     def _select(self):
         t = super(PurchaseReport,self)._select()
-        t += ", array_agg(rel_etiq.account_analytic_tag_id) as analytic_tag_ids"
+        t += ", array_agg(rel_etiq.account_analytic_tag_id) as analytic_tag_ids, array_agg( aat.name || ', ')::varchar as analytic_tag_ids_name"
         return t
 
 #    def _group_by(self):
@@ -28,7 +28,9 @@ class PurchaseReport(models.Model):
     def _from(self):
         t = super(PurchaseReport,self)._from()
         t += """
-            left join account_analytic_tag_purchase_order_line_rel rel_etiq on rel_etiq.purchase_order_line_id = l.id """
+            left join account_analytic_tag_purchase_order_line_rel rel_etiq on rel_etiq.purchase_order_line_id = l.id 
+            left join account_analytic_tag aat on aat.id = rel_etiq.account_analytic_tag_id
+            """
         return t
 
 
@@ -37,8 +39,10 @@ class SaleReport(models.Model):
 
 #    account_analytic_id = fields.Many2one("account.analytic.account",string="Cuenta Analítica")
     analytic_tag_ids = fields.Many2many("account.analytic.tag",string="Etiquetas Analíticas")
+    analytic_tag_ids_name = fields.Char(string="Etiquetas Analíticas")
 
     def _query(self, with_clause='', fields={}, groupby='', from_clause=''):
         fields['analytic_tag_ids'] = ", array_agg(rel_etiq.account_analytic_tag_id) as analytic_tag_ids"
-        from_clause += 'left join account_analytic_tag_sale_order_line_rel rel_etiq on rel_etiq.sale_order_line_id = l.id'
+        fields['analytic_tag_ids_name'] = "array_agg( aat.name || ', ')::varchar as analytic_tag_ids_name"
+        from_clause += 'left join account_analytic_tag_sale_order_line_rel rel_etiq on rel_etiq.sale_order_line_id = l.id left join account_analytic_tag aat on aat.id = rel_etiq.account_analytic_tag_id'
         return super(SaleReport, self)._query(with_clause, fields, groupby, from_clause)
