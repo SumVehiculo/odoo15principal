d9f3203a3983920738c75fe2b81f0d89bbb255eb
Bertha Maribel Chávez Salazar <73680458+Bertha-cs@users.noreply.github.com>
2023-11-08 11:04:54 -0500

    Add files via upload

---
+++ prorratear_en/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Prorratear en cambiar a solo valor',
+    'version': '1.0.0',
+    'author': 'IT GRUPO, berth',
+    'depends': ['landed_cost_it'],
+    'data': [
+    ],
+    'license': 'LGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
