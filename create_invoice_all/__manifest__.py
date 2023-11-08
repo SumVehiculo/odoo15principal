f5fadc84b64ddc5c28d2c95194cb6955af770bde
alessso124 <55262969+alessso124@users.noreply.github.com>
2023-11-08 17:20:23 -0500

    fix raise

--- create_invoice_all/__manifest__.py
+++ create_invoice_all/__manifest__.py
@@ -6,7 +6,7 @@
     'description': """Modulo que limita los botones de crear factura en modulos de ventas y compras""",
     'version': '1.0',
     'summary': 'Modificaciones personalizadas para purchase y sale',
-    'depends': ['sale','purchase'],
+    'depends': ['account'],
     'data': [
         'views/function.xml',
     ],
