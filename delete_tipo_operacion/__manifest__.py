1c6d3270a03ede13ff7c1935a8fce432664e6a78
Bertha Maribel Chávez Salazar <73680458+Bertha-cs@users.noreply.github.com>
2023-11-08 10:38:49 -0500

    Add files via upload

---
+++ delete_tipo_operacion/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Eliminar tipo de operacion",
+    'author': 'ITGRUPO, berth',
+    'version': '1.0',
+    'depends': ['account'],
+    'data': [
+        'views/account_move.xml',
+    ],
+    'demo': [],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
