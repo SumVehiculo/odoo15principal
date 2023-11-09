a8955ac0d8ad6790faac7827af03cf4c14f9ca18
Bertha Maribel Chávez Salazar <73680458+Bertha-cs@users.noreply.github.com>
2023-11-09 13:54:47 -0500

    Add files via upload

---
+++ kardex_formato_sunat12_cm_it/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- encoding: utf-8 -*-
+{
+    'name': 'Kardex Formato Sunat V12',
+    'version': '1.0',
+    'author': 'ITGRUPO-COMPATIBLE-BO',
+    'website': '',
+    'category': 'account',
+    'depends': ['kardex_valorado_it'],
+    'description': """KARDEX FORMATO SUNAT V12""",
+    'demo': [],
+    'data': [
+        'wizard/make_kardex_view.xml',
+    ],
+    'auto_install': False,
+    'installable': True
+}
