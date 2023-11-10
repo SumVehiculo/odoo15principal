e1bb52133d352de68c40d6de054e37555f388f73
Bertha Maribel Chávez Salazar <73680458+Bertha-cs@users.noreply.github.com>
2023-11-10 11:09:50 -0500

    Add files via upload

---
+++ groups_button_draft_account/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "ARestringir Button Draft de la factura",
+
+    'summary': """""",
+    'author': "ITGRUPO,berth",
+    'category': 'stock',
+    'version': '0.1',
+    'depends': ['sale','account','purchase'],
+    'data': [
+        'security/security.xml',
+        'views/account_move.xml',
+    ],
+    'demo': [],
+}
