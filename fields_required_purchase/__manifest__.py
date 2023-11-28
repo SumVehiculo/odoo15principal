c49e06cc20faa82b29ace44c633e30a0b336173a
Bertha Maribel Chávez Salazar <73680458+Bertha-cs@users.noreply.github.com>
2023-11-28 10:49:57 -0500

    Add files via upload

---
+++ fields_required_purchase/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'obligatorios',
+    'version': '1.0.0',
+    'author': 'IT GRUPO, berth',
+    'depends': ['purchase'],
+    'data': [
+        'views/fields.xml'
+    ],
+    'license': 'LGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
