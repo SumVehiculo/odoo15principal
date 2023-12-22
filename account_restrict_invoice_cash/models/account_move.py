a36fa4849f87d3d6d32140269b4ee153fcb1a28c
SEBASTIAN MOISES LORICO LOPEZ <59961777+5Moises@users.noreply.github.com>
2023-12-22 08:46:13 -0500

    update

--- account_restrict_invoice_cash/models/account_move.py
+++ account_restrict_invoice_cash/models/account_move.py
@@ -6,22 +6,33 @@ class AccountMove(models.Model):
 	_inherit = 'account.move'
 
 	@api.model
-	def search(self, args, offset=0, limit=None, order=None, count=False):
-		user = self.env.user		
-		t = super(AccountMove, self).search(args, offset, limit, order, count=count)
-		bu = user.has_group('account_restrict_invoice_cash.group_invoice_only')
-		if any(move.move_type in ["in_invoice"] for move in t) and bu:
-			args = [('create_uid', '=', self.env.user.id)] + args		
+	def search(self, args, offset=0, limit=None, order=None, count=False):		
+		if self.env.user.has_group("account_restrict_invoice_cash.group_invoice_only"):
+			args = [('create_uid', '=', self.env.user.id)] + args
+		t = super(AccountMove, self).search(args, offset, limit, order, count=count)		
 		return t
 
+		
+
+
+class account_bank_statement(models.Model):
+	_inherit = 'account.bank.statement'
 
 	@api.model
-	def name_search(self, name, args=None, operator='ilike', limit=100):
-		args = args or []
-		recs = self.browse()
-		if name:
-			recs = self.search([('create_uid','=', self.env.user.id),
-			('name',operator,name)] + args, limit=limit)
-		else:
-			recs = self.search([] + args, limit=limit)
-		return recs.name_get()
+	def search(self, args, offset=0, limit=None, order=None, count=False):
+		if self.env.user.has_group("account_restrict_invoice_cash.group_invoice_only"):
+			opt = 0
+			for condition in args:
+				if isinstance(condition,list)and len(condition)==3:					
+					field, operator, value = condition					
+					#['&', ['journal_id.type', '=', 'cash'], ['journal_id.check_surrender', '=', False]]
+					if field ==	'journal_id.type' and operator == '=' and value == 'cash':		
+						opt+= 1
+					elif field ==	'journal_id.check_surrender' and operator == '=' and value == False:
+						opt+= 1
+			if opt==2:
+				args = [('create_uid', '=', self.env.user.id)] + args
+		t = super(account_bank_statement, self).search(args, offset, limit, order, count=count)		
+		return t
+
+		
