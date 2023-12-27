5b01796d6c4400177c7d156060f63d34e5d123ea
Bertha Maribel Chávez Salazar <73680458+Bertha-cs@users.noreply.github.com>
2023-12-27 11:40:00 -0500

    Add files via upload

---
+++ tree_report_stock_quantity/__manifest__.py
@@ -0,0 +1,10 @@
+# -*- encoding: utf-8 -*-
+{
+	'name': 'Vista tree de Inventario pronosticado',
+	'author': 'ITGRUPO,berth',
+	'depends': ['stock_enterprise'],
+	'auto_install': False,
+	'demo': [],
+	'data':	['views/report_stock_quantity.xml'],
+	'installable': True
+}
