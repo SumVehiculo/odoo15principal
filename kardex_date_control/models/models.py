5ac45c8da219c750a71e5927a300748ad2dbb0c2
alessso124 <55262969+alessso124@users.noreply.github.com>
2024-01-03 10:32:08 -0500

    control

--- kardex_date_control/models/models.py
+++ kardex_date_control/models/models.py
@@ -10,8 +10,8 @@ class StockPicking(models.Model):
 
 	@api.model
 	def create(self, vals):
-		if not self.kardex_date:
-			if self.state == 'draft':
-				raise('No tiene permisos de Edicion del Kardex, ya que el Albarán esta en estado borrador')
 		t = super(StockPicking, self).create(vals)
+		if not t.kardex_date:
+			if t.state == 'draft':
+				raise UserError('No tiene permisos de Edicion del Kardex, ya que el Albarán esta en estado borrador')
 		return t
