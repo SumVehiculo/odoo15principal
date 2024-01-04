9fb5e84d025362287ab59b8394039f2c13e5227b
Bertha Maribel Chávez Salazar <73680458+Bertha-cs@users.noreply.github.com>
2024-01-04 09:58:33 -0500

    Add files via upload

---
+++ report_almacen/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "REPORTE DE ALMACEN",
+    'summary': """
+        REPORTE DE ALMACEN
+    """,
+    'description': """
+       REPORTE DE ALMACEN - FECHA EFECTIVA Y FECHA KARDEX
+    """,
+    'author': "ITGROUP,berth",
+    'website': "",
+    'category': '',
+    'version': '1.0',
+    'depends': [
+        'stock',
+    ],
+    'data': [
+        'views/albaran.xml'
+    ],
+    'application': True,
+    'license': 'LGPL-3',
+}
