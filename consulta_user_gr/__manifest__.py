b355930082a6dae16bd2968dcfa3268a261d87e9
Bertha Maribel Chávez Salazar <73680458+Bertha-cs@users.noreply.github.com>
2024-01-10 16:43:38 -0500

    Add files via upload

---
+++ consulta_user_gr/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- encoding: utf-8 -*-
+{
+	'name': 'Consulta Usuario',
+	
+	'author': 'ITGRUPO,berth',
+	'depends': ['base','account'],
+	'version': '1.0',
+	'auto_install': False,
+	'demo': [],
+	'data':	[
+        'security/ir.model.access.csv',
+		'wizards/consulta_users.xml'],
+	'installable': True,
+	'license': 'LGPL-3'
+}
