cfd686e1c43e7b0641bdeac438016a52067ca5d9
Bertha Maribel Chávez Salazar <73680458+Bertha-cs@users.noreply.github.com>
2024-01-11 10:51:48 -0500

    Add files via upload

---
+++ purchase_report/models/sale_order.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class purchase_order_line(models.Model):
+	_inherit = 'purchase.order.line'
+	
+	cod_proveedor = fields.Char('cod_sum', compute='compute_cod_proveedor')
+	
+	@api.depends('product_id')
+	def compute_cod_proveedor(self):
+		for i in self:
+			i.cod_proveedor = ""
+			for lp in i.product_id.seller_ids:
+				if lp.name.id == i.order_id.partnert_id.id:
+					i.cod_proveedor = lp.product_code
+            
