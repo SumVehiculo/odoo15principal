2c1134297b83481e41c83620d21a37be1d7f4b24
alessso124 <55262969+alessso124@users.noreply.github.com>
2024-01-22 10:42:23 -0500

    board?

---
+++ board_is_coupled/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Desacoplayo grafico",
+    'author': 'ITGRUPO, Alessandro Pelayo Mollocondo Medrano',
+    'category': 'Board',
+    'description': """Permite que la tabla grafica cuando se guarde en los tableros este se encuentre desacoplado""",
+    'version': '1.0',
+    'summary': 'Modificaciones personalizadas para product',
+    'depends': ['board'],
+    'data': [
+    ],
+    'demo': [],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
---
+++ board_is_coupled/models/models.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api
+
+# Definir la clase del tablero
+class Board(models.Model):
+	_inherit = 'board.board'
+
+	# Agregar el campo "is_coupled"
+	graph_id = fields.Many2one('board.graph', string='Gráfico', readonly=True)
+	is_coupled = fields.Boolean(string='Acoplamiento', default=True)
+
+	# Sobrescribir el método "save"
+	@api.multi
+	def save(self):
+		# Establecer el valor del campo "graph_id" a NULL
+		for board in self:
+			if not board.is_coupled:
+				board.graph_id = None
+
+		# Guardar el tablero
+		super(Board, self).save()
