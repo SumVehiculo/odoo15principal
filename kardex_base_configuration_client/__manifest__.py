a4c92f44315f135111430e330f2ed85c10797a46
JeanPierreOdooController <jeanpierreluqueluna@gmail.com>
2024-02-28 19:11:15 -0500

    Add files via upload

---
+++ kardex_base_configuration_client/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Test',
+    'version': '1.0',
+    'description': 'Test',
+    'author': 'ITGRUPO-TEST',
+    'license': 'LGPL-3',
+    'category': 'Sumtec',
+    'auto_install': False,
+    'depends': [
+        'base'
+    ],
+    'data': [
+    ],
+    'installable': True
+}
