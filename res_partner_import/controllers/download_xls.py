814c2bd4262cc961f6aef3522b4e3f2bded0e220
Jhorel Revilla Calderon <jhorel.revilla@ucsp.edu.pe>
2024-04-10 09:44:35 -0500

    [fix] controller error ->res_partner_import

--- res_partner_import/controllers/download_xls.py
+++ res_partner_import/controllers/download_xls.py
@@ -14,7 +14,7 @@ class Download_xls(http.Controller):
 	@http.route('/web/binary/download_partner_import_template_s', type='http', auth="public")
 	def download_partner_import_template(self, **kw):
 
-		invoice_xls = request.env['ir.attachment'].search([('name','=','partner_import_template_s.xlsx')])
+		invoice_xls = request.env['ir.attachment'].sudo().search([('name','=','partner_import_template_s.xlsx')])
 		filecontent = invoice_xls.datas
 		filename = 'Plantilla Importacion Partner.xlsx'
 		filecontent = base64.b64decode(filecontent)
