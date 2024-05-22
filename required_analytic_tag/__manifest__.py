04fdb5a42bdad84b11817d5a838116fed535c067
Jhorel Revilla Calderon <jhorel.revilla@ucsp.edu.pe>
2024-05-22 12:13:13 -0500

    [ADD] required_analytic_tag

---
+++ required_analytic_tag/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- encoding:utf-8 -*- 
+{
+    'name': 'Etiqueta Analitica Obligatoria',
+    'version': '1.0',
+    'description': '',
+    'author': 'ITGRUPO, Jhorel Revilla',
+    'license': 'LGPL-3',
+    'depends': [
+        'sale',
+        'stock'
+    ],
+    'data': [
+        'views/sale_order.xml'
+    ],
+    'auto_install': False,
+    'application': False
+}
