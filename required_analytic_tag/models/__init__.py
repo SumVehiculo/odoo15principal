4559ee286372d8177a7f0c77b768e2712ed8c181
Jhorel Revilla Calderon <jhorel.revilla@ucsp.edu.pe>
2024-05-24 12:14:40 -0500

    [remove] analytic_tag_restriction

--- required_analytic_tag/models/__init__.py
+++ required_analytic_tag/models/__init__.py
@@ -1,4 +1,4 @@
 from . import account_analytic_tag
 # from . import account_move_line
-from . import sale_order_line
-from . import stock_move
+# from . import sale_order_line
+# from . import stock_move
