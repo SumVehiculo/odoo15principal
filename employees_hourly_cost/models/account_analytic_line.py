6b3d83058a2f4624a11fb8d261cf137b2a42e515
Jhorel Revilla Calderon <jhorel.revilla@ucsp.edu.pe>
2024-06-19 13:20:33 -0500

    [ADD] hourly cost

--- employees_hourly_cost/models/account_analytic_line.py
+++ employees_hourly_cost/models/account_analytic_line.py
@@ -10,12 +10,30 @@ class AccountAnalyticLine(models.Model):
         store=True
     )
     
+    hourly_cost = fields.Float(
+        'Costo Por Hora',
+        compute="_compute_hourly_cost",
+        store=True
+    )
+    
+    @api.depends('employee_id')
+    def _compute_hourly_cost(self):
+        for rec in self:
+            related_employee = self.env['hr.employee.hourly.cost'].search([
+                ('employee_id','=', rec.employee_id.id)
+            ])
+            if not related_employee:
+                rec.hourly_cost=0.0
+                continue
+            rec.hourly_cost = related_employee.cost
+    
+    @api.depends('unit_amount','employee_id')
     def _compute_total_cost_per_hour(self):
         for rec in self:
             related_employee = self.env['hr.employee.hourly.cost'].search([
                 ('employee_id','=', rec.employee_id.id)
             ])
             if not related_employee:
-                rec.total_cost_per_hour=0.00
-                return
+                rec.total_cost_per_hour=0.0
+                continue
             rec.total_cost_per_hour = round(related_employee.cost  * round(rec.unit_amount,2), 2)
