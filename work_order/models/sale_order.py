dc348a0898069b67d511959dada9f04aeb713895
Jhorel Revilla Calderon <jhorel.revilla@ucsp.edu.pe>
2024-06-20 12:56:25 -0500

    [update] xml view, [fix] sale pick

--- work_order/models/sale_order.py
+++ work_order/models/sale_order.py
@@ -5,4 +5,12 @@ class SaleOrder(models.Model):
     _inherit="sale.order"
     
     def action_confirm(self):
-        pass
+        new_picks = self.picking_ids
+        res = super().action_confirm()
+        new_picks = self.picking_ids - new_picks
+        for pick in new_picks:
+            for line in pick.move_ids_without_package:
+                line.write({
+                    'work_order_id': line.sale_line_id.work_order_id.id
+                })
+        return res
