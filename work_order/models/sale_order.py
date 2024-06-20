ca9b24266b4c61d2035170a85fb63568f57095f7
Jhorel Revilla Calderon <jhorel.revilla@ucsp.edu.pe>
2024-06-20 12:59:26 -0500

    Update sale_order.py

--- work_order/models/sale_order.py
+++ work_order/models/sale_order.py
@@ -5,12 +5,13 @@ class SaleOrder(models.Model):
     _inherit="sale.order"
     
     def action_confirm(self):
-        new_picks = self.picking_ids
-        res = super().action_confirm()
-        new_picks = self.picking_ids - new_picks
-        for pick in new_picks:
-            for line in pick.move_ids_without_package:
-                line.write({
-                    'work_order_id': line.sale_line_id.work_order_id.id
-                })
-        return res
+        for res in self:
+            new_picks = res.picking_ids
+            rec = super().action_confirm()
+            new_picks = res.picking_ids - new_picks
+            for pick in new_picks:
+                for line in pick.move_ids_without_package:
+                    line.write({
+                        'work_order_id': line.sale_line_id.work_order_id.id
+                    })
+            return rec
