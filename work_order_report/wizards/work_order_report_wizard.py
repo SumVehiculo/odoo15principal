0f56ecc79e4f4b049cbbd6c0c69c2417adbfb7ce
Jhorel Revilla Calderon <jhorel.revilla@ucsp.edu.pe>
2024-06-21 18:51:56 -0500

    [ADD] detallitos :gift:

--- work_order_report/wizards/work_order_report_wizard.py
+++ work_order_report/wizards/work_order_report_wizard.py
@@ -80,12 +80,40 @@ class WorkOrderReportWizard(models.TransientModel):
             worksheet.merge_range(row, 0, row, 4, detail, formats['detail'])
             row+=1
         header_details=None
-        row+=3
-        row=self.sale_invoiced_data(row,worksheet,formats)
-        row+=3
-        row=self.invoiced_expense_data(row,worksheet,formats)
-        row+=3
-        row=self.warehouse_item_date(row,worksheet,formats)
+        row+=7
+        
+        row,sale_invoice_total=self.sale_invoiced_data(row,worksheet,formats)
+        worksheet.merge_range(row, 4, row, 6, "Total Lineas de Ventas Facturadas", formats.get('detail'))
+        worksheet.write(row, 9, sale_invoice_total, formats.get('detail'))        
+        row+=2
+        
+        row,invoiced_expenses_total=self.invoiced_expense_data(row,worksheet,formats)
+        worksheet.merge_range(row, 4, row, 6, "Total Lineas de Gastos Facturados", formats.get('detail'))
+        worksheet.write(row, 9, invoiced_expenses_total, formats.get('detail'))        
+        row+=2
+        
+        row,warehouse_item_total=self.warehouse_item_data(row,worksheet,formats)
+        worksheet.merge_range(row, 4, row, 6, "Total Articulos de Almacen", formats.get('detail'))
+        worksheet.write(row, 9, warehouse_item_total, formats.get('detail'))        
+        
+        # Header
+        worksheet.merge_range(3, 4, 3, 6, "Total Lineas de Ventas Facturadas", formats.get('detail'))
+        worksheet.write(3, 9, sale_invoice_total, formats.get('detail'))
+        
+        worksheet.merge_range(4, 4, 4, 6, "Total Lineas de Gastos Facturados", formats.get('detail'))
+        worksheet.write(4, 9, invoiced_expenses_total, formats.get('detail'))
+        
+        worksheet.merge_range(5, 4, 5, 6, "Total Articulos de Almacen", formats.get('detail'))
+        worksheet.write(5, 9, warehouse_item_total, formats.get('detail'))
+        
+        total_net=(sale_invoice_total+invoiced_expenses_total+warehouse_item_total)
+        worksheet.merge_range(5, 4, 5, 6, "NETO", formats.get('detail'))
+        worksheet.write(5, 9, total_net, formats.get('detail'))
+        
+        total_sale_percentage = (abs(total_net)/sale_invoice_total)*100
+        worksheet.merge_range(6, 4, 6, 6, "% NETO / VENTAS", formats.get('detail'))
+        worksheet.write(6, 9, total_sale_percentage, formats.get('detail'))
+        
         
         column_widths = [15,15,15,30,30,15,20,15,15,15,15]
         for i, width in enumerate(column_widths):
@@ -96,6 +124,7 @@ class WorkOrderReportWizard(models.TransientModel):
         return self.env['popup.it'].get_file(filename,base64.encodestring(b''.join(f.readlines())))
     
     def sale_invoiced_data(self, row, worksheet, formats):
+        total_sale_invoice=0
         query= f"""
             SELECT 
                 vst1.fecha,
@@ -162,10 +191,12 @@ class WorkOrderReportWizard(models.TransientModel):
             worksheet.write(row, 8, data['nro_comprobante'] if data['nro_comprobante'] else '', formats.get('base'))
             worksheet.write(row, 9, data['soles'] if data['soles'] else '', formats.get('base'))
             worksheet.write(row, 10, data['dollars'] if  data['dollars'] else '', formats.get('base'))
+            total_sale_invoice += (data['soles'] if data['soles'] else 0)
             row+=1
-        return row
+        return row,total_sale_invoice
     
     def invoiced_expense_data(self, row, worksheet, formats):
+        total_invoiced_expenses=0
         query= f"""
             SELECT 
                 vst1.fecha,
@@ -232,30 +263,49 @@ class WorkOrderReportWizard(models.TransientModel):
             worksheet.write(row, 8, data['nro_comprobante'] if data['nro_comprobante'] else '', formats.get('base'))
             worksheet.write(row, 9, data['soles'] if data['soles'] else '', formats.get('base'))
             worksheet.write(row, 10, data['dollars'] if  data['dollars'] else '', formats.get('base'))
+            total_invoiced_expenses+= (data['soles'] if data['soles'] else 0)
             row+=1
-        return row
+        return row,total_invoiced_expenses
     
-    def warehouse_item_date(self, row, worksheet, formats):
+    def warehouse_item_data(self, row, worksheet, formats):
+        warehouse_item_total=0
         query= f"""
             SELECT 
-                fechax AS kardex_date,
-                serial AS kardex_serial,
-                nro AS kardex_nro,
-                numdoc_cuadre AS kardex_doc,
-                name AS kardex_partner,
-                operation_type AS kardex_op,
-                new_name AS kardex_product,
-                default_code AS kardex_prod_cod,
-                unidad AS kardex_unit,
-                ingreso AS kardex_income,
-                salida AS kardex_release
+                total.fechax AS kardex_date,
+                total.serial AS kardex_serial,
+                total.nro AS kardex_nro,
+                total.numdoc_cuadre AS kardex_doc,
+                total.name AS kardex_partner,
+                total.operation_type AS kardex_op,
+                total.kardex_op_name as kardex_op_name,
+                total.new_name AS kardex_product,
+                total.default_code AS kardex_prod_cod,
+                total.unidad AS kardex_unit,
+                total.ingreso,
+                total.salida,
+                
+                total.product_id,
+                total.location_id,
+                total.origen_usage,
+                total.destino_usage,
+                total.debit,
+                total.credit
+
+                
             FROM 
                 (SELECT 
                     vst_kardex_sunat.*,
                     vst_kardex_sunat.fecha - interval '5' hour as fechax,
-                    np.new_name
+                    np.new_name,
+                    tok.name as kardex_op_name,
+                    sl_o.usage as origen_usage , 
+                    sl_d.usage as destino_usage
                 FROM vst_kardex_fisico_valorado AS vst_kardex_sunat
                     LEFT JOIN stock_move sm ON sm.id = vst_kardex_sunat.stock_moveid
+	                LEFT JOIN stock_picking sp ON sp.id = sm.picking_id
+                    LEFT JOIN type_operation_kardex tok ON tok.id = sp.type_operation_sunat_id
+                    LEFT JOIN stock_location sl_o ON sl_o.id = sm.location_id
+					LEFT JOIN stock_location sl_d ON sl_d.id = sm.location_dest_id
                     LEFT JOIN (
                         SELECT 
                             t_pp.id, 
@@ -294,9 +344,8 @@ class WorkOrderReportWizard(models.TransientModel):
             'COD. PRODUCTO',
             'DOC. ALMACEN',
             'CANT.',
-            'COSTO UNIT. SOLES',
-            'SOLES',
-            'DÓLARES',
+            'COSTO PROM.',
+            'COSTO TOTAL',
         ]
         for count,label in enumerate(header_list):
             worksheet.write(
@@ -308,17 +357,71 @@ class WorkOrderReportWizard(models.TransientModel):
         row+=1
         self.env.cr.execute(query)
         data_list = self.env.cr.dictfetchall()
+        cprom_data={}
         for data in data_list:
+            # CALCULO DE COSTO PROMEDIO
+            
+            llave = (data['product_id'],data['location_id'])
+            cprom_acum = [0,0]
+            
+            if llave in cprom_data:
+                cprom_acum = cprom_data[llave]
+            else:
+                cprom_data[llave] = cprom_acum
+                
+            cprom_act_antes = cprom_data[llave][1] / cprom_data[llave][0] if cprom_data[llave][0] != 0 else 0
+            
+            data_temp = {'origen':data['origen_usage'] or '','destino':data['destino_usage'] or ''}
+            
+            if data['ingreso'] or data['debit']:
+                if (data_temp['origen'] == 'internal' and data_temp['destino'] == 'internal') or (data_temp['origen'] == 'transit' and data_temp['destino'] == 'internal'):
+                    cprom_acum[0] = cprom_acum[0] + (data['ingreso'] if data['ingreso'] else 0) -  (data['salida'] if data['salida'] else 0)
+                    cprom_acum[1] = cprom_acum[1] + (data['debit'] if data['debit'] else 0) -  (data['credit'] if data['credit'] else 0)
+                else:	
+                    cprom_acum[0] = cprom_acum[0] + (data['ingreso'] if data['ingreso'] else 0) -  (data['salida'] if data['salida'] else 0)
+                    cprom_acum[1] = cprom_acum[1] + (data['debit'] if data['debit'] else 0) -  (data['credit'] if data['credit'] else 0)
+            else:
+                if (data_temp['origen'] == 'internal' and data_temp['destino'] == 'supplier'):
+                    cprom_acum[0] = cprom_acum[0] -  (data['salida'] if data['salida'] else 0)
+                    cprom_acum[1] = cprom_acum[1] - (data['debit'] if data['debit'] else 0) - ( (data['credit'] if data['credit'] else 0) * (data['salida'] if data['salida'] else 0) )
+                else:
+                    if data['salida']:
+                        cprom_acum[0] = cprom_acum[0] + (data['ingreso'] if data['ingreso'] else 0) -  (data['salida'] if data['salida'] else 0)
+                        cprom_acum[1] = cprom_acum[1] - (data['salida'] if data['salida'] else 0)*(cprom_act_antes if cprom_act_antes else 0)
+                    else:
+                        cprom_acum[0] = cprom_acum[0] + (data['ingreso'] if data['ingreso'] else 0) -  (data['salida'] if data['salida'] else 0)
+                        cprom_acum[1] = cprom_acum[1] - (data['credit'] if data['credit'] else 0)
+            cprom_acum[1] = cprom_acum[1] if cprom_acum[0] > 0 else 0
+            cprom_act = cprom_acum[1] / cprom_acum[0] if cprom_acum[0] != 0 else 0
+            
+            
+            
+            
+            
+            
+            # Fecha
             worksheet.write(row, 0, data['kardex_date'] if data['kardex_date'] else '', formats.get('base'))
-            worksheet.write(row, 1, data['kardex_serial'] if data['kardex_serial'] else '', formats.get('base'))
-            worksheet.write(row, 2, data['kardex_nro'] if data['kardex_nro'] else '', formats.get('base'))
-            worksheet.write(row, 3, data['kardex_doc'] if data['kardex_doc'] else '', formats.get('base'))
-            worksheet.write(row, 4, data['kardex_partner'] if data['kardex_partner'] else '', formats.get('base'))
-            worksheet.write(row, 5, data['kardex_op'] if data['kardex_op'] else '', formats.get('base'))
-            worksheet.write(row, 6, data['kardex_product'] if data['kardex_product'] else '', formats.get('base'))
-            worksheet.write(row, 7, data['kardex_prod_cod'] if data['kardex_prod_cod'] else '', formats.get('base'))
-            worksheet.write(row, 8, data['kardex_unit'] if data['kardex_unit'] else '', formats.get('base'))
-            worksheet.write(row, 9, data['kardex_income'] if data['kardex_income'] else '', formats.get('base'))
-            worksheet.write(row, 10, data['kardex_release'] if data['kardex_release'] else '', formats.get('base'))
+            # T. OP.
+            worksheet.write(row, 1, data['kardex_op'] if data['kardex_op'] else '', formats.get('base'))
+            # T. OP.(nombre)
+            worksheet.write(row, 2, data['kardex_op_name'] if data['kardex_op_name'] else '', formats.get('base'))
+            # Partner
+            worksheet.write(row, 3, data['kardex_partner'] if data['kardex_partner'] else '', formats.get('base'))
+            # Producto
+            worksheet.write(row, 4, data['kardex_product'] if data['kardex_product'] else '', formats.get('base'))
+            # Cod Prod
+            worksheet.write(row, 5, data['kardex_prod_cod'] if data['kardex_prod_cod'] else '', formats.get('base'))
+            # Doc Almacen
+            worksheet.write(row, 6, data['kardex_doc'] if data['kardex_doc'] else '', formats.get('base'))
+            # Cantidad
+            quantity = (data['ingreso'] if data['ingreso'] else 0) - (data['salida'] if data['salida'] else 0)
+            worksheet.write(row, 7, quantity, formats.get('base'))
+            # Costo Promedio
+            worksheet.write(row, 8, cprom_act, formats.get('base'))
+            # Costo Total
+            total_cost=round(quantity * cprom_act,2)
+            worksheet.write(row, 9, total_cost, formats.get('base'))
+            warehouse_item_total += total_cost
             row+=1
-        return row
+            
+        return row,warehouse_item_total
