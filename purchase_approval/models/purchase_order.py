2d42e47547bf55ed45d27252ad2138e1485ee410
Jhorel Revilla Calderon <jhorel.revilla@ucsp.edu.pe>
2024-06-21 11:59:25 -0500

    [add] unconfirm

--- purchase_approval/models/purchase_order.py
+++ purchase_approval/models/purchase_order.py
@@ -15,10 +15,24 @@ class PurchaseOrder(models.Model):
         copy=False
     )
     
+    
+    unconfirmed_by = fields.Many2one(
+        'res.users',
+        string='Desaprobado por',
+        copy=False
+    )           
+    date_unconfirmed = fields.Datetime(
+        'Fecha de Desaprobación',
+        copy=False
+    )
+    
+    custom_approve = fields.Boolean('Aprobacion customizada', default=False)
+    
+    
     def button_confirm(self):
         res = super().button_confirm()
         for rec in self:
-            if not rec.confirmed_by:
+            if not rec.custom_approve:
                 raise UserError("La compra necesita ser aprobada. ")
         return res
         
@@ -27,5 +41,17 @@ class PurchaseOrder(models.Model):
             raise UserError(f"Usted no tiene suficientes permisos para aprobar esta compra.")
         self.write({
             'confirmed_by' : datetime.now() - timedelta(hours=5),
-            'date_confirmed' : self.env.user.id
+            'date_confirmed' : self.env.user.id,
+            'custom_approve': True
+        })
+
+    def button_custom_unconfirm(self):
+        if not self.env.user.has_group("purchase_approval.purchase_approval_group"):
+            raise UserError(f"Usted no tiene suficientes permisos para desaprobar esta compra.")
+        if self.state != 'draft':
+            raise UserError(f"Solo se puede desaprobar compras en estado Borrador")
+        self.write({
+            'unconfirmed_by' : datetime.now() - timedelta(hours=5),
+            'date_unconfirmed' : self.env.user.id,
+            'custom_approve': False
         })
