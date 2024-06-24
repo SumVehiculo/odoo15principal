f23a2a5bfc769c6aaa95251f115c864a406e48e9
Glenda Julia <glenda.merma@tecsup.edu.pe>
2024-06-24 15:03:52 -0500

    new module

---
+++ account_fix_sum/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- encoding: utf-8 -*-
+{
+	'name': 'Correcciones Contabilidad SUM',
+	'category': 'account',
+	'author': 'ITGRUPO,Glenda Julia Merma Mayhua',
+	'depends': ['account_fields_it'],
+	'version': '1.0',
+	'description':"""
+	- Correcciones Contabilidad para SUM
+	""",
+	'auto_install': False,
+	'demo': [],
+	'data':	[
+        'views/account_bank_statement.xml',
+        'views/account_move.xml'
+	],
+	'installable': True,
+	'license': 'LGPL-3'
+}
