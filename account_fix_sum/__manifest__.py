40b74a2d13a2e3832a47960066a7ef82c263898e
Glenda Julia <glenda.merma@tecsup.edu.pe>
2024-06-24 13:56:08 -0500

    ver

---
+++ account_fix_sum/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- encoding: utf-8 -*-
+{
+	'name': 'Correcciones Contabilidad SUM',
+	'category': 'account',
+	'author': 'ITGRUPO,Glenda Julia Merma Mayhua',
+	'depends': ['account_fields_it'],
+	'version': '1.0',
+	'description':"""
+	- Correcciones Contabilidad para SUM
+	""",
+	'auto_install': False,
+	'demo': [],
+	'data':	[
+        'views/account_bank_statement.xml'
+	],
+	'installable': True,
+	'license': 'LGPL-3'
+}
---
+++ account_fix_sum/models/account_bank_statement.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api
+from odoo.exceptions import UserError, ValidationError
+import re
+
+class AccountBankStatement(models.Model):
+	_inherit = 'account.bank.statement'
+	
+	sequence_number = fields.Char(string='Secuencia')
+
+	@api.model
+	def create(self,vals):
+		if self.env.context.get('journal_type') == 'cash' and self.env.context.get('journal_check_surrender'):
+			id_seq = self.env['ir.sequence'].search([('name','=','Rendiciones Tesoreria'),('company_id','=',self.env.company.id)], limit=1)
+			if not id_seq:
+				id_seq = self.env['ir.sequence'].create({'name':'Rendiciones Tesoreria','implementation':'no_gap','active':True,'prefix':'REN-','padding':6,'number_increment':1,'number_next_actual' :1, 'company_id': self.env.company.id})
+			sequ = id_seq._next()
+			vals['sequence_number'] = sequ		
+			vals['name'] = sequ
+
+		t = super(AccountBankStatement,self).create(vals)
+		return t
