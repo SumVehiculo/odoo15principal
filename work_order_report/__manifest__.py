07220111d2a1de0a5f83c09ee4d285be8a30c1f5
Jhorel Revilla Calderon <jhorel.revilla@ucsp.edu.pe>
2024-06-25 12:59:03 -0500

    Update __manifest__.py

--- work_order_report/__manifest__.py
+++ work_order_report/__manifest__.py
@@ -6,12 +6,13 @@
     'author': 'ITGRUPO, Jhorel Revilla',
     'license': 'LGPL-3',
     'depends': [
-        'work_order'
+        'work_order',
+        'employees_hourly_cost'
     ],
     'data': [
         'security/ir.model.access.csv',
         'wizards/work_order_report_wizard.xml',
-        
+        'views/account_analytic_line.xml',
     ],
     'auto_install': False,
     'application': False
