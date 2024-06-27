29b04234b29ae73d6dd30291f617ff10d2fcb4ec
Jhorel Revilla Calderon <jhorel.revilla@ucsp.edu.pe>
2024-06-27 14:33:13 -0500

    [ADD] bank_statement_tree_memory

---
+++ bank_statement_tree_memory/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- encoding: utf-8 -*-
+{
+    'name': 'Campo Memoria en Rendiciones',
+    'version': '1.0',
+    'description': 'Agrega el campo memoria en la vista lista de rendiciones.',
+    'author': 'ITGRUPO, Jhorel Revilla',
+    'license': 'LGPL-3',
+    'depends': [
+        'account_fields_it'
+    ],
+    'data': [
+        'views/account_bank_statement.xml'
+    ],
+    'auto_install': False,
+    'application': False
+}
