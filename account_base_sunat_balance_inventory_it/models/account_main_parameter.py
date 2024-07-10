f4e3bbcbae61dc73d38163c6730e7c5c0288e06a
Glenda Julia <glenda.merma@tecsup.edu.pe>
2024-07-10 17:04:14 -0500

    sub

--- account_base_sunat_balance_inventory_it/models/account_main_parameter.py
+++ account_base_sunat_balance_inventory_it/models/account_main_parameter.py
@@ -2,8 +2,8 @@
 
 from odoo import models, fields, api
 
-class MainParameter(models.Model):
-	_inherit = 'main.parameter'
+class AccountMainParameter(models.Model):
+	_inherit = 'account.main.parameter'
 	
 	use_balance_inventory_kardex = fields.Boolean(string='Usar Kardex',default=False)
 	catalog_fs_bi = fields.Char(string=u'Catálogo de Estados Financieros')
