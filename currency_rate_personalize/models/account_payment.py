7f52dae4ab7ab96f6efed6e263409cc3ccf07480
Glenda Julia <glenda.merma@tecsup.edu.pe>
2024-07-11 17:23:28 -0500

    ver

--- currency_rate_personalize/models/account_payment.py
+++ currency_rate_personalize/models/account_payment.py
@@ -16,10 +16,10 @@ class AccountPayment(models.Model):
 class AccountPaymentRegister(models.TransientModel):
 	_inherit = 'account.payment.register'
 
-	#@api.onchange('payment_date','currency_id')
-	#def _get_currency_rate(self):
-	#	if not self.is_personalized_change:
-	#		currency_id = self.env.ref('base.USD') if self.currency_id == self.company_id.currency_id else self.currency_id
-	#		cu_rate = self.env['res.currency.rate'].search([('name','=',self.payment_date),('currency_id','=',currency_id.id),('company_id','=',self.company_id.id)],limit=1)
-	#		if cu_rate:
-	#			self.type_change = cu_rate.sale_type
+	@api.onchange('payment_date','currency_id')
+	def _get_currency_rate(self):
+		if not self.is_personalized_change:
+			currency_id = self.env.ref('base.USD') if self.currency_id == self.company_id.currency_id else self.currency_id
+			cu_rate = self.env['res.currency.rate'].search([('name','=',self.payment_date),('currency_id','=',currency_id.id),('company_id','=',self.company_id.id)],limit=1)
+			if cu_rate:
+				self.type_change = cu_rate.sale_type
