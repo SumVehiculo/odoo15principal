b3eb760794f2277212fdf1fa273d3ec53377adc9
Jhorel Revilla Calderon <jhorel.revilla@ucsp.edu.pe>
2024-07-12 12:09:52 -0500

    [ADD] project_tags_rule

---
+++ project_tags_rule/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- encoding:utf-8 -*-
+{
+    'name': 'Regla en etiquetas de Proyecto',
+    'version': '1.0',
+    'description': 'Crea la regla en etiquetas de proyectos',
+    'author': 'ITGRUPO, Jhorel Revilla',
+    'license': 'LGPL-3',
+    'depends': [
+        'project'
+    ],
+    'data': [
+        'views/ir_rule.xml'
+    ],
+    'auto_install': False,
+    'application': False
+}
