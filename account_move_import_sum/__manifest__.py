ec25fad6285fd2b29b7b14f4a8b86dd6ab40608f
Glenda Julia <glenda.merma@tecsup.edu.pe>
2024-07-17 10:06:04 -0500

    import

---
+++ account_move_import_sum/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- encoding: utf-8 -*-
+{
+	'name': 'Importador de Apuntes Contables para SUM',
+	'category': 'account',
+	'author': 'ITGRUPO,Glenda Julia Merma Mayhua',
+	'depends': ['account_move_import_it'],
+	'version': '1.0',
+	'description':"""
+	Modulo para importar Apuntes Contables personalizacion
+	""",
+	'auto_install': False,
+	'demo': [],
+	'data':	[
+		'data/attachment_sample.xml'
+		],
+	'installable': True,
+	'license': 'LGPL-3'
+}
