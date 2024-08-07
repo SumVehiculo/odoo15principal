323484ce27a6dd8c7cb3fc27757f4411bba890b4
Jhorel Revilla Calderon <jhorel.revilla@ucsp.edu.pe>
2024-08-07 09:59:36 -0500

    [update] avoid write account analytic

--- required_analytic_tag/models/account_analytic_account.py
+++ required_analytic_tag/models/account_analytic_account.py
@@ -8,5 +8,10 @@ class AccountAnalyticAccount(models.Model):
     @api.model
     def create(self, vals):
         if not self.env.user.has_group("required_analytic_tag.create_analytic_account_group"):
-            raise UserError("Solo lo usuarios del grupo: 'Permiso Crear Cuenta Analitica' pueden crear cuentas analiticas.")
+            raise UserError("Solo lo usuarios del grupo: 'Permiso Crear/Editar Cuenta Analitica' pueden crear cuentas analiticas.")
         return super().create(vals)
+    
+    def write(self, vals):
+        if not self.env.user.has_group("required_analytic_tag.create_analytic_account_group"):
+            raise UserError("Solo lo usuarios del grupo: 'Permiso Crear/Editar Cuenta Analitica' pueden crear cuentas analiticas.")
+        return super().write(vals)
