69b087e535af2460f3834bc5042382923d15e2e2
SEBASTIAN MOISES LORAICO LOPEZ <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>
2024-08-20 16:12:55 -0500

    update

--- kardex_account_journal_entry_advance/models/type_operation_kardex.py
+++ kardex_account_journal_entry_advance/models/type_operation_kardex.py
@@ -5,5 +5,5 @@ from odoo import models, fields, api
 class TypeOperationKardex(models.Model):
 	_inherit = 'type.operation.kardex'
 	
-	account_id = fields.Many2one('account.account',string=u'Cuenta')
+	account_id = fields.Many2one('account.account',string=u'Cuenta',company_dependent=True, domain="[('deprecated', '=', False), ('company_id', '=', current_company_id)]")
 	category_account = fields.Boolean(string=u'Usar cuenta Categoria',default=False)
