102c803807288f1d3fd3321e600a2bc33f9ceddf
Jhorel Revilla Calderon <jhorel.revilla@ucsp.edu.pe>
2024-11-11 15:33:25 -0500

    [ADD] invoice_extra_fields

---
+++ invoice_tree_extra_fields/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- encoding:utf-8 -*-
+{
+    'name': 'Campos Extra en La vista de Facturas',
+    'version': '1.0',
+    'description': 'Campos Extra en La vista de Facturas',
+    'author': 'ITGRUPO, Jhorel Revilla',
+    'license': 'LGPL-3',
+    'depends': [
+        'l10n_pe_edi_extended'
+    ],
+    'data': [
+        'views/account_move.xml'
+    ],
+    'auto_install': False,
+    'application': False
+}
