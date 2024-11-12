14f5b8cfbd32559f99fa98719467aa949546b7ab
Jhorel Revilla Calderon <jhorel.revilla@ucsp.edu.pe>
2024-11-12 16:14:55 -0500

    [update] report

--- work_order_report/models/general_work_order_report_total.py
+++ work_order_report/models/general_work_order_report_total.py
@@ -8,7 +8,7 @@ class GeneralWorkOrderReportTotal(models.Model):
 
     project_id = fields.Many2one('project.project', string='OT')
     client_id = fields.Many2one('res.partner', string='Cliente OT')
-    tag_ids = fields.Many2many('project.tags', string='Etiquetas')
+    tag_names = fields.Text(string='Etiquetas')
     start_date = fields.Date('Fecha Inicio del Proyecto')
     end_date = fields.Date('Fecha Fin del Proyecto')
     sale_invoices_total = fields.Float('Total de Lineas de Ventas Facturadas')
@@ -22,10 +22,7 @@ class GeneralWorkOrderReportTotal(models.Model):
         self.env.cr.execute(f"""
             CREATE OR REPLACE view sale_commercial_report as (
 				SELECT row_number() OVER () AS id, data.* FROM(
-                    {self.get_with_sql()}
-                    {self.get_select_sql()}
-                    {self.get_from_sql()}
-                    {self.get_where_sql()}
+                    {self.get_sql()}
                 ) AS data
             )
         """)
@@ -37,11 +34,13 @@ class GeneralWorkOrderReportTotal(models.Model):
             'view_type': 'form',
         }
         
-    
+    def get_sql(self):
+        return self.get_with_sql() + self.get_select_sql() + self.get_from_sql() + self.get_where_sql()
+
     def sql_sale_totals(self):
         return f"""
             SELECT 
-                SUM(vst1.balance) * - 1  AS sale_invoice_total,
+                SUM(vst1.balance) * - 1  AS total,
                 aml.work_order_id AS work_order_id
             FROM 
                 get_diariog(
@@ -68,7 +67,7 @@ class GeneralWorkOrderReportTotal(models.Model):
     def sql_purchase_totals(self):
         return f"""
             SELECT 
-                SUM(vst1.balance) * - 1  AS expenses_total,
+                SUM(vst1.balance) * - 1  AS total,
                 aml.work_order_id AS work_order_id
             FROM 
                 get_diariog(
@@ -101,7 +100,14 @@ class GeneralWorkOrderReportTotal(models.Model):
     def get_kardex_totals(self):
         return f"""
             SELECT 
-                SUM((COALESCE(total.ingreso, 0) - COALESCE(total.salida, 0)) * COALESCE(total.unit_price, 0)) AS kardex_total,
+                ROUND(
+                    SUM(
+                        (
+                            COALESCE(total.ingreso, 0) - COALESCE(total.salida, 0)
+                        ) * COALESCE(total.unit_price, 0)
+                    )::numeric ,
+                    2
+                ) AS total,
                 total.work_order_id
             FROM (
                 SELECT 
@@ -128,7 +134,7 @@ class GeneralWorkOrderReportTotal(models.Model):
     def get_hourly_cost_totals(self):
         return f"""
         SELECT
-            SUM(aal.total_cost_per_hour) * -1 AS total_cost_per_hour,
+            ROUND(CAST(SUM(aal.total_cost_per_hour) * -1 AS numeric),2) AS total,
             aal.project_id as work_order_id
         FROM 
             account_analytic_line AS aal
@@ -145,6 +151,19 @@ class GeneralWorkOrderReportTotal(models.Model):
         GROUP BY
             aal.project_id
         """
+
+    def get_project_tag(self):
+        return """
+        SELECT 
+            string_agg(pt.name, ', ') AS tags,
+            pp_pt_rel.project_project_id AS project_id
+        FROM 
+            project_project_project_tags_rel AS pp_pt_rel
+            LEFT JOIN project_tags AS pt ON pt.id = pp_pt_rel.project_tags_id
+        GROUP BY
+            pp_pt_rel.project_project_id
+        """
+
     
     def get_with_sql(self):
         return f"""
@@ -152,7 +171,8 @@ class GeneralWorkOrderReportTotal(models.Model):
                 invoice_totals AS({self.sql_sale_totals()}),
                 expenses_totals AS({self.sql_purchase_totals()}),
                 kardex_totals AS({self.get_kardex_totals()}), 
-                hourly_cost_totals AS ({self.get_hourly_cost_totals()})
+                hourly_cost_totals AS ({self.get_hourly_cost_totals()}),
+                fixed_project_tags AS ({self.get_project_tag()})
         """
     
     def get_select_sql(self):
@@ -160,6 +180,7 @@ class GeneralWorkOrderReportTotal(models.Model):
             SELECT
                 pp.id AS project_id,
                 pp.partner_id AS client_id,
+                fpt.tags AS tag_names,
                 pp.date_start AS start_date,
                 pp.date AS end_date,
                 COALESCE(it.total,0) AS sale_invoices_total,
@@ -192,6 +213,7 @@ class GeneralWorkOrderReportTotal(models.Model):
             LEFT JOIN expenses_totals AS et ON et.work_order_id = pp.id
             LEFT JOIN kardex_totals AS kt ON kt.work_order_id = pp.id
             LEFT JOIN hourly_cost_totals AS hct ON hct.work_order_id = pp.id
+            LEFT JOIN fixed_project_tags AS fpt ON fpt.project_id = pp.id
         """
     
     def get_where_sql(self):
