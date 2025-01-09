b1b96eefdfffff8ced95124d06b262d62ff78ecf
Jhorel Revilla <jhorel.revilla99@gmail.com>
2025-01-09 18:04:04 -0500

    [fix] tracking

---
+++ grupo_solo_ver_albaranes/models/kardex_fisico_it.py
@@ -0,0 +1,12 @@
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
+
+class KardexFisicoIt(models.Model):
+    _inherit = 'kardex.fisico.it'
+
+    invoice_id = fields.Many2one(
+        'account.move',
+        'Factura',
+        domain=[('move_type', 'in', ['out_invoice','in_invoice','out_refund','in_refund'])],
+        tracking=True
+    )    
