81aaed91bde0ae1b51207d3016c02fbe54725135
alessso124 <55262969+alessso124@users.noreply.github.com>
2025-03-12 18:20:41 -0500

    Desarrollo / Campos en comentarios de Documentos electronicos (#35683)

---
+++ account_move_new_fields/models/models.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+from odoo import api, fields, models
+from odoo.exceptions import UserError
+from odoo import tools
+
+
+class AccountMove(models.Model):
+	_inherit = 'account.move'
+
+	payment_method_it = fields.Selection([('01','Efectivo'),
+										  ('02','Tarjeta'),
+										  ('03','Deposito'),
+										  ('04','Transferencia'),
+										  ('05','Otros'),
+										  ],string='Metodo de pago', tracking=True)
+	operation_number_it = fields.Text(string='N° de operacion', tracking=True)
+	payment_date_it = fields.Date(string='Fecha de pago', tracking=True)
