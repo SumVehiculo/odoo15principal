d7b97c9f5d5b6c22a8d9f3b4987330cf2abd918a
alessso124 <55262969+alessso124@users.noreply.github.com>
2025-03-12 18:11:21 -0500

    update

---
+++ account_move_new_fields/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Agregar campos en facturas",
+    'author': 'ITGRUPO, Alessandro Pelayo Mollocondo Medrano',
+    'category': 'Account',
+    'description': """Modulo que permite agregar unas campos en account_move""",
+    'version': '1.0',
+    'summary': 'Modificaciones personalizadas para account',
+    'depends': ['account'],
+    'data': [
+        'views/views.xml',
+    ],
+    'demo': [],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
