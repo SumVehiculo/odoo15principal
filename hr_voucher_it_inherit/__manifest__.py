38c061acfaf09f59ec3bc125f32466b845e7c819
informatico10 <galire@unsa.edu.pe>
2025-06-13 19:11:57 -0500

    update planillas rrhh

---
+++ hr_voucher_it_inherit/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- encoding: utf-8 -*-
+{
+	'name': 'Hr Voucher IT Inherit',
+	'category': 'hr',
+	'author': 'ITGRUPO-HR',
+	'depends': ['hr_voucher_it'],
+	'version': '1.0',
+	'description':"""
+	Modulo para Boletas de pago en Nominas - SUM VEHICULOS
+	""",
+	'auto_install': False,
+	'demo': [],
+	'data':	[
+		# 'views/hr_payslip.xml',
+	],
+	'installable': True,
+	'license': 'LGPL-3',
+}
